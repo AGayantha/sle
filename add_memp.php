35321de08bb0e79ee072fc6fc75ce5dc095c61ba
Avishka Ariyarathna <avishkaariyarathna@gmail.com>
2024-03-23 15:07:57 +0530

    second commit

--- add_memp.php
+++ add_memp.php
@@ -6,13 +6,14 @@ if (isset($_SESSION["name"])) {
     $add_name = $_SESSION["name"];
     $date = $_POST['date'];
     $unit = $_POST['unit'];
+    $reject = $_POST['reject'];
     $tariff= 3702;
     $revenue = $tariff * $unit;
 
-    $sql = "insert into memp(user,date, unit,tariff,Revenue) values(?,?,?,?,?)";
+    $sql = "insert into memp(user,date, unit,reject,tariff,Revenue) values(?,?,?,?,?,?)";
     $statment = $connection->prepare($sql);
 
-    $statment->bind_param("sssss",$add_name, $date, $unit,$tariff, $revenue);
+    $statment->bind_param("ssssss",$add_name, $date, $unit,$reject,$tariff, $revenue);
 
     $statment->execute();
 
